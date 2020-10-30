7bc5ccbd4443528db4d5a6800e4b2d28d5c2aae4
aleksey-pro <anxieter@yandex.ru>
2020-10-30 13:01:26 +0300

    Modified price placeholer

--- src/components/Common/FormElements/Text.tsx
+++ src/components/Common/FormElements/Text.tsx
@@ -7,6 +7,7 @@ type PropsType = {
     id: string
     name: string
     placeholder?: string | undefined
+    placeholderIn?: boolean | undefined
     description?: string | undefined
     message?: string | undefined
     required?: boolean | undefined
@@ -14,23 +15,22 @@ type PropsType = {
 };
 
 export const Text = (props: PropsType) => {
-    const title = props.title;
-    const id = props.id;
-    const name = props.name;
-    const placeholder = props.placeholder;
-    const required = props.required;
-    const disabled = props.disabled;
-
+    const { title, id, name, placeholder, placeholderIn = true } = props;
+    const ph = placeholderIn ? placeholder : '';
     const [
         field,
         { error, touched },
     ] = useField({
         name: name,
     });
-
+    const input = <input type="text" {...props} placeholder={ph} {...field}/>;
     return <div className={"input-label " + name}>
         {title && <label htmlFor={id}>{title}</label>}
-        <input disabled={disabled} type="text" id={id} required={required} placeholder={placeholder} {...field}/>
+        {placeholderIn && input}
+        {!placeholderIn && <div className="input-flex">
+            {input}
+        <div className="input-cur">{placeholder}</div>
+        </div>}
         {error && touched && <div className="msg_err__container"><span className="msg_err">{error}</span></div>}
     </div>
 };
